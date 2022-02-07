29d6b57dadd268617f582f84317229512767f24f
saintsloth <dakaprielov@gmail.com>
2022-02-07 03:15:41 +0300

    i dont know...

--- src/eventListener.js
+++ src/eventListener.js
@@ -10,40 +10,44 @@ const eventListener = () => {
   const input = document.getElementById('url-input');
   const form = document.getElementById('rss-form');
   const schema = yup.object({ url: yup.string().url().required() });
-  input.addEventListener('input', (e) => {
-    watchedState.url = e.target.value;
-  });
-
-  form.addEventListener('submit', (e) => {
-    e.preventDefault();
-    const formData = new FormData(e.target);
-    const url = formData.get('url');
-    schema.isValid({ url }).then((isValid) => {
-      if (isValid) {
-        if (_.find(watchedData.feeds, (feed) => feed.link === url)) {
-          watchedFeedback.text = localeInstance.t('rssExist');
-          watchedFeedback.color = 'red';
-        } else {
-          rssParser(url)
-            .then(({ feed, posts }) => {
-              watchedData.feeds.push(feed);
-              watchedData.posts.push(...posts);
-            })
-            .then(() => {
-              watchedFeedback.text = localeInstance.t('rssSuccess');
-              watchedFeedback.color = 'green';
-            })
-            .catch(() => {
-              watchedFeedback.text = localeInstance.t('rssValidUrlNo');
-              watchedFeedback.color = 'red';
-            });
-        }
-      } else {
-        watchedFeedback.text = localeInstance.t('rssValidNo');
-        watchedFeedback.color = 'red';
-      }
+  if (input !== null) {
+    input.addEventListener('input', (e) => {
+      watchedState.url = e.target.value;
     });
-  });
+  }
+
+  if (form !== null) {
+    form.addEventListener('submit', (e) => {
+      e.preventDefault();
+      const formData = new FormData(e.target);
+      const url = formData.get('url');
+      schema.isValid({ url }).then((isValid) => {
+        if (isValid) {
+          if (_.find(watchedData.feeds, (feed) => feed.link === url)) {
+            watchedFeedback.text = localeInstance.t('rssExist');
+            watchedFeedback.color = 'red';
+          } else {
+            rssParser(url)
+              .then(({ feed, posts }) => {
+                watchedData.feeds.push(feed);
+                watchedData.posts.push(...posts);
+              })
+              .then(() => {
+                watchedFeedback.text = localeInstance.t('rssSuccess');
+                watchedFeedback.color = 'green';
+              })
+              .catch(() => {
+                watchedFeedback.text = localeInstance.t('rssValidUrlNo');
+                watchedFeedback.color = 'red';
+              });
+          }
+        } else {
+          watchedFeedback.text = localeInstance.t('rssValidNo');
+          watchedFeedback.color = 'red';
+        }
+      });
+    });
+  }
 };
 
 export default eventListener;
