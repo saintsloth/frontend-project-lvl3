85b9e808f7bf50aa777ec12e34a0275efd5b3ce5
saintsloth <dakaprielov@gmail.com>
2022-02-09 16:50:39 +0300

    again...

--- webpack.config.js
+++ webpack.config.js
@@ -17,7 +17,7 @@ function setDevTool() {  // function to set dev-tool depending on environment
 
 const config = {
   entry: {
-    index: ['babel-polyfill', './src/webpackInitAll.js'],
+    index: ['babel-polyfill', './index.js'],
   },
   output: {
     filename: isDev ? '[name].js' : '[name].[hash].js',
