d3030785d98e2fd937a689abf84978b797a912d4
saintsloth <dakaprielov@gmail.com>
2022-02-09 20:50:36 +0300

    readonly in input

--- src/button.js
+++ src/button.js
@@ -1,9 +1,13 @@
 export const buttonBlock = () => {
   const button = document.getElementById('rss-button');
   button.setAttribute('disabled', 'true');
+  const input = document.getElementById('url-input');
+  input.setAttribute('readonly', 'true');
 };
 
 export const buttonUnBlock = () => {
   const button = document.getElementById('rss-button');
   button.removeAttribute('disabled');
+  const input = document.getElementById('url-input');
+  input.removeAttribute('readonly');
 };
